cc84c914c0676b13cab5f5d52c4315b15be49e3b
sanjana-19o8 <sanjanavajrapu19@gmail.com>
2023-07-03 22:17:47 +0530

    update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,7 @@
 // Deploy our smart contract onto Testnet using ethers.js library
 
 import { ethers } from 'ethers'
-import { abi, addr} from '../contracts/election'
+import { abi, addr } from '../contracts/election'
 
 declare global {
     interface Window {
@@ -11,6 +11,16 @@ declare global {
 
 export const writeContract = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    // Request user account access
+    window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then((accounts: any) => {
+            // Handle successful account access
+            console.log('Accounts:', accounts);
+        })
+        .catch((error: any) => {
+            // Handle error
+            console.error('Error:', error);
+        });
     provider.send('eth_requestAccount', []);
     const signer = provider.getSigner();
 
